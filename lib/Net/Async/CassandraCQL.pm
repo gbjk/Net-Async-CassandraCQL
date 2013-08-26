8ec450f29b6c1819f580506250a6962d05e58575
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2013-08-26 22:21:49 +0100

    Initial empty skeleton

---
+++ lib/Net/Async/CassandraCQL.pm
@@ -0,0 +1,41 @@
+#  You may distribute under the terms of either the GNU General Public License
+#  or the Artistic License (the same terms as Perl itself)
+#
+#  (C) Paul Evans, 2013 -- leonerd@leonerd.org.uk
+
+package Net::Async::CassandraCQL;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+C<Net::Async::CassandraCQL> - use Cassandra databases with L<IO::Async> using CQL
+
+=cut
+
+=head1 SPONSORS
+
+This code was paid for by
+
+=over 2
+
+=item *
+
+Perceptyx L<http://www.perceptyx.com/>
+
+=item *
+
+Shadowcat Systems L<http://www.shadow.cat>
+
+=back
+
+=head1 AUTHOR
+
+Paul Evans <leonerd@leonerd.org.uk>
+
+=cut
+
+0x55AA;

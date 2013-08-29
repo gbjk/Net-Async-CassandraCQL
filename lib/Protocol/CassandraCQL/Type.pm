59047fea2075edda5a8a0d6a1e14929ccab7a19d
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2013-08-29 18:20:39 +0100

    Construct Type objects in subclasses to neaten ->encode/->decode dispatch

--- lib/Protocol/CassandraCQL/Type.pm
+++ lib/Protocol/CassandraCQL/Type.pm
@@ -13,7 +13,6 @@ our $VERSION = '0.01';
 use Carp;
 
 use Encode ();
-use Scalar::Util qw( blessed );
 
 use Protocol::CassandraCQL qw( :types );
 
@@ -44,12 +43,7 @@ sub from_frame
    my ( $frame ) = @_;
 
    my $typeid = $frame->unpack_short;
-   if( $typeid == TYPE_CUSTOM ) {
-      return Protocol::CassandraCQL::Type::Custom->from_frame( $frame );
-   }
-   else {
-      return Protocol::CassandraCQL::Type->new( $typeid );
-   }
+   return Protocol::CassandraCQL::Type->from_name( Protocol::CassandraCQL::typename( $typeid ) );
 }
 
 # Just for unit testing
@@ -58,15 +52,15 @@ sub from_name
    shift;
    my ( $name ) = @_;
 
-   defined( my $typeid = Protocol::CassandraCQL->can( "TYPE_$name" )->() ) or
-      croak "No such type '$name'";
-   return Protocol::CassandraCQL::Type->new( $typeid );
+   my $class = "Protocol::CassandraCQL::Type::$name";
+   die "Unrecognised type name '$name'" unless $class->can( "new" );
+   $class->new
 }
 
 sub new
 {
    my $class = shift;
-   bless [ $_[0] ], $class;
+   return bless [], $class;
 }
 
 =head1 METHODS
@@ -82,66 +76,18 @@ Returns a string representation of the type name.
 sub name
 {
    my $self = shift;
-   return Protocol::CassandraCQL::typename( $self->id );
+   return +( ( ref $self ) =~ m/::([^:]+)$/ )[0];
 }
-
-=head2 $id = $type->id
-
-Returns the basic type ID as the value of a C<TYPE_*> constant.
-
-=cut
-
-sub id
-{
-   my $self = shift;
-   return $self->[0];
-}
-
-# TODO: bind encode/decode methods at instantiation
-#  Or better yet, subclass
 
 =head2 $bytes = $type->encode( $v )
 
 Encodes the given perl data into a bytestring.
-
-=cut
-
-sub encode
-{
-   my $self = shift;
-   my ( $v ) = @_;
-
-   my $type = $self->name;
-   if( my $code = __PACKAGE__->can( "encode_$type" ) ) {
-      return $code->( $v );
-   }
-   else {
-      warn "Not sure how to encode $type";
-      return $v;
-   }
-}
 
 =head2 $v = $type->decode( $bytes )
 
 Decodes the given bytestring into perl data.
 
 =cut
-
-sub decode
-{
-   my $self = shift;
-   my ( $b ) = @_;
-
-   my $type = $self->name;
-   if( my $code = __PACKAGE__->can( "decode_$type" ) ) {
-      return $code->( $b );
-   }
-   else {
-      warn "Not sure how to decode $type";
-      # Fallback to a text-safe hexbytes representation
-      return unpack "H*", $b;
-   }
-}
 
 #      if( $typeid == TYPE_CUSTOM ) {
 #         push @col, $frame->unpack_string;
@@ -153,68 +99,95 @@ sub decode
 # Now the codecs
 
 # ASCII-only bytes
-sub encode_ASCII { $_[0] =~ m/^[\x00-\x7f]*$/ or die "Non-ASCII"; $_[0] }
-sub decode_ASCII { $_[0] }
+package Protocol::CassandraCQL::Type::ASCII;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { $_[1] =~ m/^[\x00-\x7f]*$/ or die "Non-ASCII"; $_[1] }
+sub decode { $_[1] }
 
 # 64-bit integer
-sub encode_BIGINT { pack   "q>", $_[0] }
-sub decode_BIGINT { unpack "q>", $_[0] }
+package Protocol::CassandraCQL::Type::BIGINT;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { pack   "q>", $_[1] }
+sub decode { unpack "q>", $_[1] }
 
 # blob
-sub encode_BLOB { $_[0] }
-sub decode_BLOB { $_[0] }
+package Protocol::CassandraCQL::Type::BLOB;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { $_[1] }
+sub decode { $_[1] }
 
 # true/false byte
-sub encode_BOOLEAN { pack   "C", !!$_[0] }
-sub decode_BOOLEAN { !!unpack "C", $_[0] }
+package Protocol::CassandraCQL::Type::BOOLEAN;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { pack   "C", !!$_[1] }
+sub decode { !!unpack "C", $_[1] }
 
 # counter is a 64-bit integer
-*encode_COUNTER = \&encode_BIGINT;
-*decode_COUNTER = \&decode_BIGINT;
+package Protocol::CassandraCQL::Type::COUNTER;
+use base qw( Protocol::CassandraCQL::Type::BIGINT );
 
 # Not clearly docmuented, but this appears to be an INT decimal shift followed
 # by a VARINT
-sub encode_DECIMAL {
+package Protocol::CassandraCQL::Type::DECIMAL;
+use base qw( Protocol::CassandraCQL::Type );
+use Scalar::Util qw( blessed );
+sub encode {
    require Math::BigFloat;
-   my $shift = $_[0] =~ m/\.(\d*)$/ ? length $1 : 0;
-   my $n = blessed $_[0] ? $_[0] : Math::BigFloat->new( $_[0] );
-   return pack( "L>", $shift ) . encode_VARINT( $n->blsft($shift, 10) );
+   my $shift = $_[1] =~ m/\.(\d*)$/ ? length $1 : 0;
+   my $n = blessed $_[1] ? $_[1] : Math::BigFloat->new( $_[1] );
+   return pack( "L>", $shift ) . Protocol::CassandraCQL::Type::VARINT->encode( $n->blsft($shift, 10) );
 }
-
-sub decode_DECIMAL {
+sub decode {
    require Math::BigFloat;
-   my $shift = unpack "L>", $_[0];
-   my $n = decode_VARINT( substr $_[0], 4 );
+   my $shift = unpack "L>", $_[1];
+   my $n = Protocol::CassandraCQL::Type::VARINT->decode( substr $_[1], 4 );
    return scalar Math::BigFloat->new($n)->brsft($shift, 10);
 }
 
 # IEEE double
-sub encode_DOUBLE { pack   "d>", $_[0] }
-sub decode_DOUBLE { unpack "d>", $_[0] }
+package Protocol::CassandraCQL::Type::DOUBLE;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { pack   "d>", $_[1] }
+sub decode { unpack "d>", $_[1] }
 
 # IEEE single
-sub encode_FLOAT { pack   "f>", $_[0] }
-sub decode_FLOAT { unpack "f>", $_[0] }
+package Protocol::CassandraCQL::Type::FLOAT;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { pack   "f>", $_[1] }
+sub decode { unpack "f>", $_[1] }
 
 # 32-bit integer
-sub encode_INT { pack   "l>", $_[0] }
-sub decode_INT { unpack "l>", $_[0] }
+package Protocol::CassandraCQL::Type::INT;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { pack   "l>", $_[1] }
+sub decode { unpack "l>", $_[1] }
 
-# 'text' seems to come back as 'varchar' but we'll leave them both aliased
-*encode_VARCHAR = *encode_TEXT = \&Encode::encode_utf8;
-*decode_VARCHAR = *decode_TEXT = \&Encode::decode_utf8;
+# UTF-8 text
+package Protocol::CassandraCQL::Type::VARCHAR;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode { Encode::encode_utf8 $_[1] }
+sub decode { Encode::decode_utf8 $_[1] }
+
+# 'text' seems to come back as 'varchar'
+package Protocol::CassandraCQL::Type::TEXT;
+use base qw( Protocol::CassandraCQL::Type::VARCHAR );
 
 # miliseconds since UNIX epoch as 64bit uint
-sub encode_TIMESTAMP {  pack   "Q>", ($_[0] * 1000) }
-sub decode_TIMESTAMP { (unpack "Q>", $_[0]) / 1000  }
+package Protocol::CassandraCQL::Type::TIMESTAMP;
+use base qw( Protocol::CassandraCQL::Type );
+sub encode {  pack   "Q>", ($_[1] * 1000) }
+sub decode { (unpack "Q>", $_[1]) / 1000  }
 
 # TODO: UUID
 
 # Arbitrary-precision 2s-complement signed integer
 # Math::BigInt doesn't handle signed, but we can mangle it
-sub encode_VARINT {
+package Protocol::CassandraCQL::Type::VARINT;
+use base qw( Protocol::CassandraCQL::Type );
+use Scalar::Util qw( blessed );
+sub encode {
    require Math::BigInt;
-   my $n = blessed $_[0] ? $_[0] : Math::BigInt->new($_[0]); # upgrade to a BigInt
+   my $n = blessed $_[1] ? $_[1] : Math::BigInt->new($_[1]); # upgrade to a BigInt
 
    my $bytes;
    if( $n < 0 ) {
@@ -233,15 +206,14 @@ sub encode_VARINT {
    }
    $bytes;
 }
-
-sub decode_VARINT {
+sub decode {
    require Math::BigInt;
 
-   if( unpack( "C", $_[0] ) >= 0x80 ) {
-      return -Math::BigInt->from_hex( unpack "H*", ~$_[0] ) - 1;
+   if( unpack( "C", $_[1] ) >= 0x80 ) {
+      return -Math::BigInt->from_hex( unpack "H*", ~$_[1] ) - 1;
    }
    else {
-      return Math::BigInt->from_hex( unpack "H*", $_[0] );
+      return Math::BigInt->from_hex( unpack "H*", $_[1] );
    }
 }
 
